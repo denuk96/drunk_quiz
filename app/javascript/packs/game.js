587d09b743c6501ed7059476948d49f6df04c919
Denys <denys.taradada.triare@gmail.com>
2021-05-07 16:17:38 +0300

    qrcode for game

--- app/javascript/packs/game.js
+++ app/javascript/packs/game.js
@@ -1,17 +1,24 @@
-// import gameChannel from '../channels/game_channel'
-//
-// $( document ).on('turbolinks:load', function() {
-// 	const gameContainer = $('#game-container')
-// 	if (gameContainer.length <= 0) { return }
-//
-// 	const gameId = gameContainer.data('game-id')
-// 	const playerId = gameContainer.data('player-id')
-//
-// 	const btnStart = $('#start-game-btn')
-//
-// 	function setListener() {
-// 		btnStart.click('')
-// 	}
-//
-// 	gameChannel(gameId, {playerId})
-// })
+import QRCode from 'qrcode'
+
+$( document ).on('turbolinks:load', function() {
+	const place = $('#current-page-qr-code')
+	if (place.length <= 0) { return }
+	const text = window.location.href
+
+	const opts = {
+		errorCorrectionLevel: 'H',
+		type: 'image/jpeg',
+		quality: 0.3,
+		margin: 1,
+		color: {
+			dark:"#010599FF",
+			light:"#ffffff"
+		}
+	}
+
+	QRCode.toDataURL(text, opts,function (err, url) {
+		let image = new Image();
+		image.src = url
+		place.append(image);
+	})
+})
